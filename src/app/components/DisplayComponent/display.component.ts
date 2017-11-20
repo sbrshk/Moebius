21a2c3b219ab69a2b53174d208fe06603c511d9a
CellarD0-0r <alan.turing39@gmail.com>
2017-11-20 18:05:03 +0300

    consistent applying transforms

--- src/app/components/DisplayComponent/display.component.ts
+++ src/app/components/DisplayComponent/display.component.ts
@@ -15,6 +15,7 @@ export class DisplayComponent implements OnInit {
     private plotter: Plotter;
     private shapes: Shapes;
     private canvas: HTMLCanvasElement;
+    private currentModel: Model;
 
     constructor() {
         this.shapes = new Shapes();
@@ -25,8 +26,9 @@ export class DisplayComponent implements OnInit {
 
         this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
         this.plotter = new Plotter(this.canvas);
+        this.currentModel = this.shapes.getShape(_index);
         this.plotter.drawAxis();
-        this.plotter.drawModel(this.shapes.getShape(_index));
+        this.plotter.drawModel(this.currentModel);
     }
 
     public applyTransform(transformMatrix: Matrix, model: Model): Model | any {
@@ -43,20 +45,29 @@ export class DisplayComponent implements OnInit {
         console.log(code);
         let _transformMatrix: Matrix;
         switch (code) {
-            case 37: _transformMatrix = AffineTransform.translation(-1, 0); break;
-            case 38: _transformMatrix = AffineTransform.translation(0, 1); break;
-            case 39: _transformMatrix = AffineTransform.translation(1, 0); break;
-            case 40: _transformMatrix = AffineTransform.translation(0, -1); break;
+            case 37: _transformMatrix = AffineTransform.translation(-0.1, 0); break;
+            case 38: _transformMatrix = AffineTransform.translation(0, 0.1); break;
+            case 39: _transformMatrix = AffineTransform.translation(0.1, 0); break;
+            case 40: _transformMatrix = AffineTransform.translation(0, -0.1); break;
+            case 65: _transformMatrix = AffineTransform.rotationAngle(0.1); break;
+            case 68: _transformMatrix = AffineTransform.rotationAngle(-0.1); break;
+            case 87: _transformMatrix = AffineTransform.reflectionX(); break;
+            case 83: _transformMatrix = AffineTransform.reflectionY(); break;
+            case 32: _transformMatrix = AffineTransform.reflectionCenter(); break;
+            case 81: _transformMatrix = AffineTransform.scaling(0.9, 0.9); break;
+            case 69: _transformMatrix = AffineTransform.scaling(1.1, 1.1); break;
             default: _transformMatrix = AffineTransform.identity(); break;
         }
-        let _index = parseInt(document.cookie[document.cookie.length - 1], 10);
-        let _currentModel: Model = this.shapes.getShape(_index);
+        let _currentModel: Model = this.currentModel;
         let _transformedModel = new Model(_currentModel.getVerticesCount(), _currentModel.getEdgesCount());
         _transformedModel.setEdges(_currentModel.getEdges());
         _transformedModel.setVertices(this.applyTransform(_transformMatrix, _currentModel).getVertices());
         // console.log(_transformedModel.getVertices().cells);
         this.plotter.drawAxis();
         this.plotter.drawModel(_transformedModel);
-        
+        this.currentModel = _transformedModel;
+    }
+    public onMouseWheel(delta: number | any): void {
+        console.log(delta);
     }
 }
