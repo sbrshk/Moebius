efda4074353dceecb25ceb29fd3f8a74edd9d68a
CellarD0-0r <alan.turing39@gmail.com>
2017-12-07 17:41:15 +0300

    display3d component init

--- src/app/Shapes/Shapes3d.ts
+++ src/app/Shapes/Shapes3d.ts
@@ -1,1 +1,4 @@
 import { Matrix } from '../Matrix/Matrix';
+import { Model3d } from '../Model/Model3d';
+
+export class Shapes3d {}
---
+++ src/app/components/3d/Display3dComponent/display3d.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Matrix } from '../../../Matrix/Matrix';
+import { Model3d } from '../../../Model/Model3d';
+import { Shapes3d } from '../../../Shapes/Shapes3d';
+import { Plotter3d } from '../../../Plotter/Plotter3d';
+
+@Component({
+    selector: 'display3d-component',
+    templateUrl: 'display3d.component.html'
+})
+
+export class Display3dComponent {}
