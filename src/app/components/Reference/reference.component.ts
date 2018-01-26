6496b981a52328886d4ff80bd787ad8c2af7feec
CellarD0-0r <alan.turing39@gmail.com>
2018-01-26 23:57:34 +0300

    ref init

--- src/app/components/Reference/reference.component.ts
+++ src/app/components/Reference/reference.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'reference',
-    templateUrl: 'reference.component.html'
+    templateUrl: 'reference.component.html',
+    styleUrls: ['../../../assets/sass/components/ref.sass']
 })
 
 export class ReferenceComponent {}
